38052b6ce29accb669053bebf834c0d2feeb85f5
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2024-06-15 11:52:11 +0200

    vue

--- bdd/script.sql
+++ bdd/script.sql
@@ -60,6 +60,3 @@ CREATE TABLE facture(
    FOREIGN KEY(id_vehicule) REFERENCES vehicule(id)
 );
 
-CREATE OR REPLACE VIEW info_vehicule AS
-SELECT v.*, f.date_debut, f.date_fin, a.nom nom_assurance, t.nom nom_type FROM vehicule v LEFT JOIN facture f on v.id = f.id_vehicule 
-LEFT JOIN assurance a ON a.id = f.id_assurance LEFT JOIN type_vehicule t on t.id = v.id_type;
---
+++ bdd/vue.sql
@@ -0,0 +1,3 @@
+CREATE OR REPLACE VIEW info_vehicule AS
+SELECT v.*, f.date_debut, f.date_fin, a.nom nom_assurance, t.nom nom_type FROM vehicule v LEFT JOIN facture f on v.id = f.id_vehicule 
+LEFT JOIN assurance a ON a.id = f.id_assurance LEFT JOIN type_vehicule t on t.id = v.id_type;
