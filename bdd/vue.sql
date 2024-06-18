0eb13861db90af8c09930a185027dc23862dca13
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2024-06-19 00:24:14 +0200

    mise a jour vue info_vehicule

--- bdd/vue.sql
+++ bdd/vue.sql
@@ -1,3 +1,3 @@
 CREATE OR REPLACE VIEW info_vehicule AS
-SELECT v.*, f.date_debut, f.date_fin, a.nom nom_assurance, t.nom nom_type FROM vehicule v LEFT JOIN facture f on v.id = f.id_vehicule 
-LEFT JOIN assureur a ON a.id = f.id_assureur LEFT JOIN type_vehicule t on t.id = v.id_type;
+SELECt v.*, a.nom nom_assurance, f.date_fin, t.nom nom_type from vehicule v JOIN assureur a on v.id_assureur = a.id
+left join facture f on f.id_assureur = a.id left join type_vehicule t on t.id = v.id_type;
