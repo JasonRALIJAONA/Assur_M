7ab6fc185282921c85920a6c657bbc563b726a48
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2024-06-19 00:25:39 +0200

    inscription vehicule

--- assets/js_client/inscription_vehicule2.js
+++ assets/js_client/inscription_vehicule2.js
@@ -3,38 +3,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const radios = document.getElementsByName('assureur');
     const radios_carburants = document.getElementsByName('type_moteur');
-    const carburant = document.getElementsByName('carburant_box');
+    const carburant = document.getElementById('carburant_box');
     var box_usage = document.getElementById('box_usage');
+    var box_options = document.getElementById('box_options');
     const annee_fabrication = document.getElementById('annee_fabrication_box');
     const option_annee_fabrication = document.getElementById('annee_fabrication');
     var id_assurance;
 
+    var liste_usage;
+    var liste_options;
 
-// ASSUREUR ON_CHANGE
+
+    // ASSUREUR ON_CHANGE
     for (let radio of radios) {
         radio.addEventListener('change', function () {
             if (this.checked) {
                 id_assurance = this.value;
-                display_annee_fabrication(this.value);
-                get_and_display_Usage();
+                display_annee_fabrication();
+                get_options();
+                get_Usage();
+
             }
         });
     }
 
-// CARBURANT ON_CHANGE
+    // ANNEE FABRICATION ON_CHANGE
+    option_annee_fabrication.addEventListener('change', function () {
+        // Faire apparaitre le carburant_box
+        carburant.style.display = 'block';
+
+    });
+
+    // CARBURANT ON_CHANGE
     for (let radio of radios_carburants) {
         radio.addEventListener('change', function () {
-            // get_and_display_Usage();
-
+            // SUBMIT BOUTON ACTIVER
+            document.getElementById('bouton_box').style.display = 'block';
         });
     }
 
-// ANNEE FABRICATION ON_CHANGE
-    
+
 
 
     // FAIRE APPARAITRE LE CHAMP ANNEE DE FABRICATION:
-    
+
 
     function display_annee_fabrication() {
         var liste_annee = {};
@@ -66,7 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
         optionVide.value = '';
         optionVide.textContent = 'Sélectionner une année de fabrication';
         annee_option.appendChild(optionVide);
-        console.log(liste_annee);
 
         for (let option of liste_annee) {
             let optionElement = document.createElement('option');
@@ -77,13 +88,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         annee_fabrication.style.display = 'block';
     }
-
-    
-
-    option_annee_fabrication.addEventListener('change', function () {
-        // Faire apparaitre le carburant_box
-        carburant.style.display = 'block';
-    });
 
     function display_carburant() {
         var liste_carburant = {};
@@ -104,8 +108,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // MAKA USAGE REHETRA
-    function get_and_display_Usage() {
-        var liste_usage = {};
+    function get_Usage() {
         $.ajax({
             url: baseUrl + 'donnee_controller/get_usages',
             method: 'GET',
@@ -115,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
             dataType: 'json',
             success: function (data) {
                 liste_usage = data.liste_usage;
-                display_usage(liste_usage);
+                display_usage();
 
             },
             error: function (xhr, status, error) {
@@ -125,11 +128,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
-    function display_usage(liste_usage) {
+    function display_usage() {
 
-        
+
+        box_usage.style.display = 'block';
+        box_usage.innerHTML = '';
+
+        var dive = document.createElement('div');
+        dive.className = 'col-md-12';
+
+        dive.style.color = 'white';
+        dive.textContent = 'Usage:';
+
+        box_usage.appendChild(dive);
+
         for (let row of liste_usage) {
-            console.log(row);
             var div = document.createElement('div');
             div.className = 'form-check col-md-5';
 
@@ -137,7 +150,7 @@ document.addEventListener("DOMContentLoaded", function () {
             input.className = 'form-check-input';
             input.type = 'radio';
             input.name = 'mode_usage';
-            input.id = 'mode_usage1';
+            input.id = 'mode_usage_' + row['id'];
             input.value = row['id'];
             input.setAttribute('data-bs-toggle', 'popover');
             input.setAttribute('data-bs-custom-class', 'custom-popover');
@@ -156,8 +169,137 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     }
 
+    function get_options() {
+        $.ajax({
+            url: baseUrl + 'donnee_controller/get_options',
+            method: 'GET',
+            data: {
+                id_assureur: id_assurance
+            },
+            dataType: 'json',
+            success: function (data) {
+                liste_options = data.liste_options;
+                display_options();
+            },
+            error: function (xhr, status, error) {
+                console.error('Erreur lors de la récupération des carburants:', status, error);
+            }
+        });
+    }
 
+    function display_options() {
+        box_options.style.display = 'block';
+        box_options.innerHTML = '';
 
+        var dive = document.createElement('div');
+        dive.className = 'col-md-12';
 
+        dive.style.color = 'white';
+        dive.textContent = 'Options';
+
+        box_options.appendChild(dive);
+
+        for (let row of liste_options) {
+            var div = document.createElement('div');
+            div.className = 'form-check col-md-5';
+
+            var input = document.createElement('input');
+            input.className = 'form-check-input';
+            input.type = 'radio';
+            input.name = 'option';
+            input.id = 'cotisation1';
+            input.value = row['id'];
+            input.setAttribute('data-bs-toggle', 'popover');
+            input.setAttribute('data-bs-custom-class', 'custom-popover_cotisation');
+            input.setAttribute('data-bs-trigger', 'hover focus');
+
+            var label = document.createElement('label');
+            label.className = 'form-check-label';
+            label.setAttribute('for', 'cotisation1');
+            label.textContent = row['nom'];
+
+            div.appendChild(input);
+            div.appendChild(label);
+
+            box_options.appendChild(div);
+        }
+    }
+
+    // *********************************************************************
 
 });
+
+(function ($) {
+
+    $(".contactForm").on("submit", async function (event) {
+        event.preventDefault();
+
+        $(".contactForm").validate().form();
+        let prix;
+        try {
+            prix = await calculer_assurance();
+        } catch (error) {
+            console.log(error);
+        }
+        Swal.fire({
+            icon: "question",
+            text: "Ce sera " + prix + " par mois, enregistrer ?",
+            showCancelButton: true,
+            confirmButtonText: 'Enregistrer',
+            cancelButtonText: 'Annuler'
+
+        }).then( async (result) => {
+            if (result.isConfirmed) {
+                // Action à réaliser lorsque l'utilisateur clique sur "Oui"
+                await enregistrer();
+                setTimeout(() => {
+                    window.location = "accueil";
+    
+                }, 1000);
+
+                // Ajoutez ici le code pour enregistrer les données ou effectuer une action
+            } else if (result.isDismissed) {
+                // Action à réaliser lorsque l'utilisateur clique sur "Non"
+                console.log("Utilisateur a annulé");
+                // Ajoutez ici le code pour annuler l'enregistrement ou effectuer une autre action
+            }
+        });
+    });
+
+    async function calculer_assurance() {
+        var prix;
+        var formData = $('.contactForm').serializeArray();
+
+        await $.ajax({
+            url: baseUrl + 'form_controller/inscription_vehicule_page2',
+            type: 'POST',
+            dataType: 'json',
+            data: $.param(formData),
+
+            success: function (response) {
+                console.log(response.prix);
+                prix = response.prix;
+            }
+        });
+        return prix;
+    }
+
+    async function enregistrer() {
+        var prix;
+        var formData = $('.contactForm').serializeArray();
+
+        await $.ajax({
+            url: baseUrl + 'form_controller/inscrire_vehicule',
+            type: 'POST',
+            dataType: 'json',
+            data: $.param(formData),
+
+            success: function (response) {
+                console.log(response);
+                // prix = response.prix;
+            }
+        });
+        return prix;
+    }
+
+})(jQuery);
